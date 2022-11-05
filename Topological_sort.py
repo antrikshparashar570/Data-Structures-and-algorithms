b3c286c7f5c99d8c59a46deb8a203cd951b4ab26
antrikshparashar570 <42244094+antrikshparashar570@users.noreply.github.com>
2022-11-05 15:26:59 +0530

    Create Topological_sort.py

---
+++ Topological_sort.py
@@ -0,0 +1,26 @@
+d = {
+		0: [2, 3],
+		1: [],
+		2: [],
+		3: [1],
+		4: [1, 2],
+		5: [0, 2]
+}
+
+def dfs(d, v, visited, stack):
+	visited[v] = True
+	for i in d[v]:
+		if not visited[i]:
+			dfs(d, i, visited, stack)
+	stack.append(v)
+	
+def topoSort(d):
+	stack = []
+	visited = [False for i in range(len(d))]
+	for key in d.keys():
+		if not visited[key]:
+			dfs(d, key, visited, stack)
+	print(stack[::-1])
+	
+topoSort(d)
+		
